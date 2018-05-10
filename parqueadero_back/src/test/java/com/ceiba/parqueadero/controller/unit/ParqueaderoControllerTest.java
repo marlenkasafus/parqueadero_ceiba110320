7962ab9f07744e24d555becf1da721c3a3561122
Santiago Cuervo <santiago.cuervo@ceiba.com.co>
2018-05-10 13:51:43 -0500

    Crea paquete de unitarias

--- parqueadero_back/src/test/java/com/ceiba/parqueadero/controller/unit/ParqueaderoControllerTest.java
+++ parqueadero_back/src/test/java/com/ceiba/parqueadero/controller/unit/ParqueaderoControllerTest.java
@@ -1,4 +1,4 @@
-package com.ceiba.parqueadero.controller;
+package com.ceiba.parqueadero.controller.unit;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
