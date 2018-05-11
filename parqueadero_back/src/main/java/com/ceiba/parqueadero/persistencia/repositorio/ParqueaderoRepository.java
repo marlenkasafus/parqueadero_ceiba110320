6c9508db864f833281ca7bc93e9397e2dae9be11
Santiago Cuervo <santiago.cuervo@ceiba.com.co>
2018-05-11 09:27:18 -0500

    Soluciona deuda tecnica sonar

--- parqueadero_back/src/main/java/com/ceiba/parqueadero/persistencia/repositorio/ParqueaderoRepository.java
+++ parqueadero_back/src/main/java/com/ceiba/parqueadero/persistencia/repositorio/ParqueaderoRepository.java
@@ -8,17 +8,17 @@ import org.springframework.stereotype.Repository;
 import com.ceiba.parqueadero.dominio.Parqueadero;
 import com.ceiba.parqueadero.dominio.enums.EstadoParqueaderoEnum;
 import com.ceiba.parqueadero.dominio.enums.TipoVehiculoEnum;
+import com.ceiba.parqueadero.persistencia.builder.ParqueaderoBuilder;
 import com.ceiba.parqueadero.persistencia.repositorio.jpa.ParqueaderoRepositoryJPA;
 
 @Repository
 public class ParqueaderoRepository {
 	
 	@Autowired
-	private ParqueaderoRepositoryJPA parqueaderoRepository;
+	private ParqueaderoRepositoryJPA parqueaderoRepositoryJPA;
 	
 	public List<Parqueadero> findByEstadoAndTipoVehiculo(EstadoParqueaderoEnum estadoParqueaderoEnum, TipoVehiculoEnum tipoVehiculoEnum){
-		parqueaderoRepository.findByEstadoAndTipoVehiculo(estadoParqueaderoEnum.getCodigo(), tipoVehiculoEnum.getCodigo());
-		return null;
+		return ParqueaderoBuilder.convertirADominio(parqueaderoRepositoryJPA.findByEstadoAndTipoVehiculo(estadoParqueaderoEnum.getCodigo(), tipoVehiculoEnum.getCodigo()));
 	}
 
 	public int getCantidadParqueaderosUtilizados(TipoVehiculoEnum tipoVehiculoEnum) {
